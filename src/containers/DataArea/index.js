69e05fb6cc3a56c88239892ac41c38a2c1284b81
Paul Han <paul.myung.han@gmail.com>
2020-05-25 18:39:08 -0700

    handleSearchChange

--- src/containers/DataArea/index.js
+++ src/containers/DataArea/index.js
@@ -63,6 +63,20 @@ export default class DataArea extends Component {
     const sortedUsers = this.state.filteredUsers.sort(compareFnc);
     this.setState({ filteredUsers: sortedUsers });
   }
+  
+  handleSearchChange = event => {
+    console.log(event.target.value);
+    const filter = event.target.value;
+    const filteredList = this.state.users.filter(item => {
+      // merge data together, then see if user input is anywhere inside
+      let values = Object.values(item)
+        .join("")
+        .toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
+    this.setState({ filteredUsers: filteredList });
+  }
+
 
   
 
