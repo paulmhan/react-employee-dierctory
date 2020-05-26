4f14dd1864c361fee5b6c95b21c5dce509e39eb4
Paul Han <paul.myung.han@gmail.com>
2020-05-25 18:38:30 -0700

    created the handle sorting function that compares two headings

---
+++ src/containers/DataArea/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import DataTable from "./DataTable";
+import Nav from "./Nav";
+import API from "../utils/API";
+import "../styles/DataArea.css";
+
+export default class DataArea extends Component {
+  state = {
+    users: [{}],
+    order: "descend",
+    filteredUsers: [{}]
+  }
+
+  headings = [
+    { name: "Image", width: "10%" },
+    { name: "Name", width: "10%" },
+    { name: "Phone", width: "20%" },
+    { name: "Email", width: "20%" },
+    { name: "DOB", width: "10%" }
+  ]
+
+  handleSort = heading => {
+    if (this.state.order === "descend") {
+      this.setState({
+        order: "ascend"
+      })
+    } else {
+      this.setState({
+        order: "descend"
+      })
+    }
+
+    const compareFnc = (a, b) => {
+      if (this.state.order === "ascend") {
+        // account for missing values
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        }
+        // numerically
+        else if (heading === "name") {
+          return a[heading].first.localeCompare(b[heading].first);
+        } else {
+          return a[heading] - b[heading];
+        }
+      } else {
+        // account for missing values
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        }
+        // numerically
+        else if (heading === "name") {
+          return b[heading].first.localeCompare(a[heading].first);
+        } else {
+          return b[heading] - a[heading];
+        }
+      }
+
+    }
+    const sortedUsers = this.state.filteredUsers.sort(compareFnc);
+    this.setState({ filteredUsers: sortedUsers });
+  }
+
+  
+
+  
+
+  render() {
+    return (
+      
+    );
+  }
+}
